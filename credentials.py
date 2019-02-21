09d30c9376e0f1f5b1fe22f1ba44f2fed9af8c6e
Amin Ghiasi Shirazi <aminghiasishirazi@Amins-MacBook-Pro-2.local>
2019-02-20 21:31:59 -0500

    Adding credentials.py

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Account details to be used with coinmarketcap API
+coinmarketcap_headers = {
+ 'Accept': 'application/json',
+ 'Accept-Encoding': 'deflate, gzip',
+ 'X-CMC_PRO_API_KEY': '',
+}
+
+# currencylayer access key
+currencylayer_access_key = ''
